3d4267a584dd3a620db674c8b6e8add5e6cc5e1a
Owen <31156070+imowen@users.noreply.github.com>
2024-10-05 20:22:36 +0700

    Update footer.tsx

--- src/components/frontend/shared/footer.tsx
+++ src/components/frontend/shared/footer.tsx
@@ -4,7 +4,7 @@ import { appConfig } from "@/config";
 export function Footer() {
   return (
     <footer className="container py-4 text-center text-sm text-muted-foreground mt-10">
-      <!--<div className="my-3 flex items-center justify-center gap-3">
+      {/*<div className="my-3 flex items-center justify-center gap-3">
         <b>Our Products: </b>
         <a href="https://www.redirectchecker.org/" className="text-primary flex items-center">
           <img alt="Redirect Checker" src="https://www.faviconextractor.com/favicon/www.redirectchecker.org" className="h-5 w-5 mr-1 rounded-full bg-secondary" width={20} height={20} />
@@ -18,7 +18,7 @@ export function Footer() {
           <img alt="Favicon Generator" src="https://www.faviconextractor.com/favicon/www.favicongenerator.io" className="h-5 w-5 mr-1 rounded-full bg-secondary" width={20} height={20} />
           <span>Favicon Generator</span>
         </a>
-      </div>-->
+      </div>*/}
       <span>
         © {new Date().getFullYear()} <a href="https://favicon.ee">{appConfig.appName}</a>. All rights reserved.
       </span>
